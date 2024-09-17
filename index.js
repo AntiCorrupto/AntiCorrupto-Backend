2ddde647d99ff7397a373c303112671c4beb531d
Mayank Aggarwal <mayankdatabase04@gmail.com>
2024-09-17 21:08:11 +0530

    Updated URL

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 dotenv.config();
 
 const corsOptions = {
-  origin: 'https://anticorrupto-frontend-c3ys1cd83-anticorruptos-projects.vercel.app',
+  origin: 'https://anticorrupto-frontend.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
